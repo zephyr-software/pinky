8e395a16a6820c3bb7ded06a22a2e539a7a7a3d7
zephyr <leonid.golenkov@gmail.com>
2024-11-24 19:08:59 +0300

    terminal colors ansi escape codes

--- utils.py
+++ utils.py
@@ -19,3 +19,12 @@ def print_pretty_ast(ast_text):
         print(' '*i, end='')
       print(ch, end='')
       newline = False
+
+class Colors:
+  WHITE     = '\033[0m'
+  BLUE      = '\033[94m'
+  CYAN      = '\033[96m'
+  GREEN     = '\033[92m'
+  YELLOW    = '\033[93m'
+  RED       = '\033[91m'
+  MAGENTA   = '\033[35m'
