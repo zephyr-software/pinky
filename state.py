0f457e45ea898dc4bcb0f7da05ef9940ce329e2a
zephyr <leonid.golenkov@gmail.com>
2024-12-16 01:54:12 +0300

    global local variables

--- state.py
+++ state.py
@@ -4,12 +4,29 @@ class Environment:
     self.parent = parent # Parent environemt (optional)
 
   def get_var(self, name):
-    #TODO:
-    pass
+    '''
+    Search the current environment and all parent environments for a variable name (return None if we dont find any)
+    '''
+    while self:
+      value = self.vars.get(name)
+      if value is not None:
+        return value
+      else:
+        self = self.parent # Look in parent environments to see if variable is defined "above"
+    return None
 
   def set_var(self, name, value):
-    #TODO:
-    pass
+    '''
+    Store a value in the environment (dynamically updating an existing name or creating a new entry in the dictionary)
+    '''
+    original_env = self
+    while self:
+      if name in self.vars:
+        self.vars[name] = value
+        return value
+      self = self.parent
+    # If we did not find the variable in the environments above, we create it in the original one
+    original_env.vars[name] = value
 
   def new_env(self):
     '''
