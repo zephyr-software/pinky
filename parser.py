f63ac63fc6b1d45345ffc7da539cd9d99500e04e
zephyr <leonid.golenkov@gmail.com>
2024-12-23 03:19:23 +0300

    max number of params

--- parser.py
+++ parser.py
@@ -214,8 +214,12 @@ class Parser:
   # <params>  ::=  <identifier> ("," <identifier> )*
   def params(self):
     params = []
+    numparams = 0
     while not self.is_next(TOK_RPAREN):
       name = self.expect(TOK_IDENTIFIER)
+      numparams += 1
+      if numparams > 255:
+        parse_error(f'Functions cannot have more than 255 parameters.', name.line)
       params.append(Param(name.lexeme, line=self.previous_token().line))
       if not self.is_next(TOK_RPAREN):
         self.expect(TOK_COMMA)
